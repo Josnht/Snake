cc4387f8887d385cb75da2c243f7de112c58b384
Josnht <phihung1262@gmail.com>
2023-04-28 23:11:25 +0700

    Add

---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<Map, ImageSource> gridToIMG = new()
+        {
+            {Map.Empty, Texture.Empty },
+            {Map.Food, Texture.Food },
+            {Map.Snake, Texture.Body }
+        };
+
+        private readonly Dictionary<Direction, int> dirRotation = new()
+        {
+            {Direction.Up, 0},
+            {Direction.Down, 180 },
+            {Direction.Left,  270},
+            { Direction.Right, 90}
+        };
+
+        private readonly int rows =15, cols =15;
+        private readonly Image[,] gridImages;
+        private State state;
+        private bool Running;
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridImages = GridSetUp();
+            state = new State (rows, cols);
+        }
+    private async Task Loading ()
+        {
+            Load();
+            await Countdown();
+            Over1ay.Visibility = Visibility.Hidden;
+            await Loop();
+            await GameOver();
+            state = new State (rows, cols);
+        }
+
+    private async void Windown_Previewkeydown (object sender, KeyEventArgs e)
+        {
+            if(Over1ay.Visibility == Visibility.Visible)
+            {
+                e.Handled= true;
+            }
+            if(!Running)
+            {
+                Running=true;
+                await Loading();
+                Running=false;
+            }
+        }
+    private void Windown_Keydown (object sender, KeyEventArgs e)
+        {
+            if (state.gameover)
+            {
+                return;
+            } switch (e.Key)
+            {
+                case Key.Left:
+                    state.ChangeDirection(Direction.Left); 
+                    break;       
+                case Key.Right:
+                    state.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    state.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    state.ChangeDirection(Direction.Down);
+                    break;
+            }
+        }
+
+    private async Task Loop ()
+        {
+            while (!state.gameover)
+            {
+                await Task.Delay(100);
+                state.Move();
+                Load();
+            }
+        }
+
+    private Image[,] GridSetUp()
+        {
+            Image[,] images = new Image[rows, cols];        
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+            for (int r= 0; r < rows; r++)
+            {
+                for(int c= 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Texture.Empty
+                    };
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
+        }
+
+    private void Load()
+    {
+            DrawGrid();
+            SnakeHead();
+            ScoreText.Text= $"SCORE {state.score}";
+    }
+    private void DrawGrid()
+    {
+        for(int r= 0; r < rows; ++r)
+        {
+            for (int c= 0;c < cols; c++)
+            {
+                Map Gridval = state.Map[r, c];
+                gridImages[r, c].Source = gridToIMG[Gridval];
+            }
+        }
+    }
+
+    private void SnakeHead()
+        {
+            Position headpos = state.HeadPosition();
+            Image image = gridImages[headpos.Row, headpos.Col];
+            image.Source = Texture.Head;
+
+            int Rotation = dirRotation[state.Dir];
+            image.RenderTransform = new RotateTransform(Rotation);
+        }
+    private async Task Countdown()
+        {
+            for (int i=0; i < 3; ++i)
+            {
+                Over1ayText.Text=i.ToString();
+                await Task.Delay(1000);
+            }
+        }
+     private async Task GameOver()
+        {
+            await Task.Delay(1000);
+            Over1ay.Visibility = Visibility.Visible;
+            Over1ayText.Text = "PRESS ANY KEY TO START";
+        }  
+    }
+}
---
+++ Snake/State.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class State
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public Map[,] Map { get; }
+        public Direction Dir { get; private set; }
+        public int score { get; private set; }
+        public bool gameover { get; private set; }
+        private readonly LinkedList<Direction> dirChange = new LinkedList<Direction>();
+        private readonly LinkedList<Position> position = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public State(int rows, int cols)
+        {
+            rows = Rows;
+            cols = Cols;
+            Map = new Map[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            Addfood();
+        }
+
+        private void AddSnake()
+            {
+            int r = Rows / 2;
+            for (int c=1; c<=3; c++)
+            {
+                Map[r, c] = Snake.Map.Snake;
+                position.AddFirst(new  Position(r, c));
+            }
+            }
+        private IEnumerable<Position> EmptyPosition()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c=0; c< Cols; c++)
+                {
+                    if (Map[r,c] == Snake.Map.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+        private void Addfood()
+        {
+            List<Position> empty = new List<Position>(EmptyPosition());
+            if (empty.Count == 0)
+            {
+                return;
+            }
+            Position pos = empty[random.Next(empty.Count)];
+            Map[pos.Row, pos.Col] = Snake.Map.Food;
+        }
+        public Position HeadPosition()
+        {
+            return position.First.Value;
+        }
+        public Position TailPosition()
+        {
+            return position.Last.Value;
+        }
+        public IEnumerable<Position> SnakePosition()
+        {
+            return position;
+        }
+        public void AddHead(Position pos)
+        {
+            position.AddFirst(pos);
+            Map[pos.Row, pos.Col] = Snake.Map.Snake;
+        }
+        public void RemoveTail()
+        {
+            Position tail = position.Last.Value;
+            Map[tail.Row, tail.Col] = Snake.Map.Empty;
+            position.RemoveLast();
+        }
+        private Direction GetLastDirection()
+        {
+            if (dirChange.Count == 0)
+            {
+                return Dir;
+            }return dirChange.Last.Value;
+        }
+        private bool CanChangeDir(Direction newDir)
+        {
+            if (dirChange.Count == 2)
+            {
+                return false;
+            }
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Oposite(); 
+        }
+        public void ChangeDirection (Direction dir)
+        {
+            if (CanChangeDir(dir))
+            {
+                dirChange.AddLast(dir);
+            }
+        }
+        private bool OutsideMap(Position pos)
+        {
+            return pos.Row <0 || pos.Row >=Rows || pos.Col <0|| pos.Col >=Cols;
+        }
+        private Map Willhit (Position headpos)
+        {
+            if (OutsideMap(headpos))
+            {
+                return Snake.Map.Outside;
+            }
+            if (headpos == TailPosition())
+            {
+                return Snake.Map.Empty;
+            }
+            return Map[headpos.Row, headpos.Col];
+        }
+        public void Move ()
+        {
+            if (dirChange.Count >0)
+            {
+                Dir = dirChange.First.Value;
+                dirChange.RemoveFirst();
+            }
+            Position headpos = HeadPosition().Translate(Dir);
+            Map hit = Willhit(headpos);
+            if (hit == Snake.Map.Outside || hit == Snake.Map.Snake)
+            {
+                gameover = true;
+            }else if (hit == Snake.Map.Empty)
+            {
+                RemoveTail();
+                AddHead(headpos);
+            }
+            else if (hit == Snake.Map.Food)
+            {
+                AddHead(headpos);
+                score++;
+                Addfood();
+            }
+        }
+    }
+}
